05bc62ffa38e3a5cffb4832d5bd38586e18c9f78
Konstantin <gritsus@gmail.com>
2017-01-10 12:01:54 +0500

    bit more

---
+++ .awesome.addition.lua
@@ -0,0 +1,5 @@
+-- windows opacity support
+-- awful.util.spawn_with_shell("xcompmgr -cF &")
+--
+-- keyboard layouts
+awful.util.spawn_with_shell("setxkbmap -model pc104 -layout us,ru -variant -option grp:alt_shift_toggle")
